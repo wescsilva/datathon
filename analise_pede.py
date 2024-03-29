0f998810a9859fbf2b80c3ba2ba4af69bb0e4ae8
Caio Delgado <caiodelgado.csd@gmail.com>
2024-03-29 19:53:07 -0300

    adicionado novos gráficos dados PEDE

--- analise_pede.py
+++ analise_pede.py
@@ -10,8 +10,11 @@ def load_pede_data():
 
     csv_content = response.content.decode('utf-8')
     csv_string_io = StringIO(csv_content)
+
+    df = pd.read_csv(csv_string_io, sep=';', decimal=".")
+    # df['IDADE_ALUNO_2020'] = df['IDADE_ALUNO_2020'].fillna(0)
     
-    return pd.read_csv(csv_string_io, sep=';')
+    return df
 
 def analise_pede():
     df = load_pede_data()
@@ -24,7 +27,7 @@ def analise_pede():
     alunos_por_ano.columns = ['index', 'values']
     fig = px.bar(alunos_por_ano, x="index", y="values", title='Quantidade de Alunos por Ano', labels=dict(values="Quantidade de Alunos", index="Ano"))
     fig.update_xaxes(tickvals=alunos_por_ano['index'])
-    fig.update_layout(title_text = 'Quantidade de Alunos por Ano', title_x = 0.35)
+    fig.update_layout(title_text = 'Quantidade de Alunos por Ano')
 
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
     
@@ -49,5 +52,75 @@ def analise_pede():
     df_porcentagem['Ponto_Virada'].fillna(0, inplace=True)
 
     fig = px.pie(values=df_porcentagem['Ponto_Virada'], names=df_porcentagem['Ano'], title = 'Alunos que atingiram o Ponto de Virada por Ano')
-    fig.update_layout(title_text = 'Alunos que atingiram o Ponto de Virada por Ano', title_x = 0.2)
+    fig.update_layout(title_text = 'Alunos que atingiram o Ponto de Virada por Ano')
     st.plotly_chart(fig, theme = 'streamlit', use_container_width = True)
+
+    # Distribuição de Notas por Disciplina em 2022
+    df_media_notas = df[['NOTA_PORT_2022', 'NOTA_MAT_2022', 'NOTA_ING_2022']].mean().reset_index()
+    df_media_notas.columns = ['Disciplina', 'Média']
+    df_media_notas = df_media_notas.replace('NOTA_PORT_2022', 'Português 2022')
+    df_media_notas = df_media_notas.replace('NOTA_MAT_2022', 'Matemática 2022')
+    df_media_notas = df_media_notas.replace('NOTA_ING_2022', 'Inglês 2022')
+    st.write()
+    fig4 = px.bar(df_media_notas, x='Disciplina', y='Média', color='Disciplina', title='Média de Notas por Disciplina (2022)')
+    st.plotly_chart(fig4)
+
+    # Desempenho dos Bolsistas em 2022
+    df_media_notas_bolsistas = df.groupby('BOLSISTA_2022').mean(['NOTA_PORT_2022', 'NOTA_MAT_2022', 'NOTA_ING_2022'])[['NOTA_PORT_2022', 'NOTA_MAT_2022', 'NOTA_ING_2022']]
+
+    nota_port = df_media_notas_bolsistas['NOTA_PORT_2022'].reset_index()
+    nota_port['Disciplina'] = 'Português'
+    nota_port.columns = ['Bolsista', 'Nota', 'Disciplina']
+    
+    nota_mat = df_media_notas_bolsistas['NOTA_MAT_2022'].reset_index()
+    nota_mat['Disciplina'] = 'Matemática'
+    nota_mat.columns = ['Bolsista', 'Nota', 'Disciplina']
+    
+    nota_ing = df_media_notas_bolsistas['NOTA_ING_2022'].reset_index()
+    nota_ing['Disciplina'] = 'Inglês'
+    nota_ing.columns = ['Bolsista', 'Nota', 'Disciplina']
+
+    df_media_notas_disciplina = pd.concat([nota_port, nota_mat, nota_ing])
+    
+    fig = px.bar(df_media_notas_disciplina, x='Disciplina', y='Nota', color='Bolsista', barmode='group',
+             labels={'Nota': 'Nota Média', 'Disciplina': 'Disciplina', 'Bolsista': 'Bolsista'},
+             title='Nota Média por Disciplina e Status de Bolsista')
+    st.plotly_chart(fig)
+
+    # Desempenho por Nível Ideal em 2022
+    fig7 = px.bar(df, x='NIVEL_IDEAL_2022', y='IAA_2022', color='NIVEL_IDEAL_2022', title='Desempenho (IAA_2022) em 2022 por Nível Ideal')
+    st.plotly_chart(fig7)
+
+    # Correlação entre Desempenho e Recomendações da Equipe em 2022
+    rec_equipe_1 = df[['REC_EQUIPE_1_2021']]
+    rec_equipe_1['Equipe'] = 'Equipe 1 2021'
+    rec_equipe_1.columns = ['Recomendação', 'Equipe']
+    
+    rec_equipe_2 = df[['REC_EQUIPE_2_2021']]
+    rec_equipe_2['Equipe'] = 'Equipe 2 2021'
+    rec_equipe_2.columns = ['Recomendação', 'Equipe']
+    
+    rec_equipe_3 = df[['REC_EQUIPE_3_2021']]
+    rec_equipe_3['Equipe'] = 'Equipe 3 2021'
+    rec_equipe_3.columns = ['Recomendação', 'Equipe']
+    
+    rec_equipe_4 = df[['REC_EQUIPE_4_2021']]
+    rec_equipe_4['Equipe'] = 'Equipe 4 2021'
+    rec_equipe_4.columns = ['Recomendação', 'Equipe']
+
+    rec_equipes = pd.concat([rec_equipe_1, rec_equipe_2, rec_equipe_3, rec_equipe_4]).reset_index()
+    rec_equipes_agrupado = rec_equipes.groupby(['Recomendação', 'Equipe']).count()['index'].reset_index()
+    
+    recomendacao_lista = rec_equipes_agrupado['Recomendação'].unique().tolist()
+    recomendacao_lista.append('Todas')
+    recomendacao_selecionada = st.selectbox('Selecione uma opção:', recomendacao_lista, index=(len(recomendacao_lista)-1))
+
+    if (recomendacao_selecionada != 'Todas'):
+        rec_equipes_agrupado_filtrado = rec_equipes_agrupado[rec_equipes_agrupado['Recomendação'] == recomendacao_selecionada]
+    else:
+        rec_equipes_agrupado_filtrado = rec_equipes_agrupado
+
+    fig = px.bar(rec_equipes_agrupado_filtrado, x='Recomendação', y='index', color='Equipe', barmode='group',
+             labels={'y': 'Quantidade de Alunos'},
+             title='Recomendação dos Alunos por Equipe (2021)')
+    st.plotly_chart(fig)
